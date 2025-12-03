0b145f70e2f581d7579ae495ad09217c296e958b
Mediams1 <bernardobdms20@gmail.com>
2025-12-02 20:25:50 -0600

    Initial commit - Tiendita React

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+import { Navbar } from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./login/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import History from "./pages/History";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <CartProvider>
+          <div className="min-h-screen bg-background">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/history" element={<History />} />
+            </Routes>
+          </div>
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
