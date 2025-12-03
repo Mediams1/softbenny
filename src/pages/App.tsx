ab3eca3abab15ae045df514b78d18358e8c88078
Mediams1 <107006257+Mediams1@users.noreply.github.com>
2025-12-03 00:54:54 -0600

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext";
-import { Navbar } from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Cart from "./pages/Cart";
-import History from "./pages/History";
+import { AuthProvider } from "../context/AuthContext";
+import { CartProvider } from "../context/CartContext";
+import { Navbar } from "../components/Navbar";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Cart from "./Cart";
+import History from "./History";
 
 function App() {
   return (
